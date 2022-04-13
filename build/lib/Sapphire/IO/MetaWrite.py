c8f6c655cc64c689805dbe8cc947f34bcdef769c
Robert Jones <52043020+JonesRobM@users.noreply.github.com>
2022-04-13 18:19:34 +0100

    Delete build/lib/Sapphire directory

--- build/lib/Sapphire/IO/MetaWrite.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep 28 10:30:45 2021
-
-@author: robert
-"""
-
--- build/lib/Sapphire/Light/__init__.py
+++
@@ -1,4 +0,0 @@
-#Your face here
-from .ClassSpec import Spectrum
-import Epsilon_DFT as dft
-import Epsilon_ExpClass as exp
